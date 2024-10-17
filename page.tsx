5753d325c674cd2bf3f25d8c602b97ac30b6dd33
Muhammad Usman Asif <164504890+Usman-Philco123@users.noreply.github.com>
2024-10-17 11:55:41 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+  <div className="bg-gray-700 text-white flex justify-center items-center m-auto h-12 font-bold">
+    <h1>This is my Home Page</h1>
+  </div>
+  );
+}
